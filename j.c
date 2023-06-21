07918384b6fd86ba2b6c587b9adf88f5397323f1
Carol091405 <paraestudos2005@gmail.com>
2023-06-21 12:44:50 -0300

    Atividade do Cap.5

--- j.c
+++ j.c
@@ -2,16 +2,20 @@
 
 int main(){
     int c,f;
-    printf("Valores em Celsius:\n");
-
+    
     for ( c = 10; c < 101; c++)
     {
+        if (c%10==0){
+        printf("Valor em Celsius:");
+        printf("%d ", c);
+        }
         if (c%10==0)
         {
-            printf("Valores em Fahrenheit:\n");
+            printf("equivale ao valor em Fahrenheit:");
             f=(((9*c)/5)+32);
             printf("%d ", f);
+            printf("\n");
         }
     }
-    
+    return 0;
 }
